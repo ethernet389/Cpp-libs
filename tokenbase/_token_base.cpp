f5a143d41f7656e7562508093015447d68fd0b70
ethernet389 <ethernet389@gmail.com>
2022-11-09 20:01:17 +0800

    Add "const&" to all methods

--- tokenbase/_token_base.cpp
+++ tokenbase/_token_base.cpp
@@ -4,13 +4,13 @@
 token_base::token_base(hash_func_t hash_function) : hash_function(hash_function) {} //Constructor by hash function
 
 
-token_base::token_base(hash_func_t hash_function, token_cont_t& tokens) //Constructor by hash function and token container
+token_base::token_base(hash_func_t hash_function, token_cont_t const& tokens) //Constructor by hash function and token container
 	: hash_function(hash_function), tokens(tokens) {} 
 
 token_base::token_base(hash_func_t hash_function, token_cont_t&& tokens) //Move constructor by hash function and token container
 	: hash_function(hash_function), tokens(std::move(tokens)) {}
 
-token_base::token_base(token_base& tb)  //Copy constructor
+token_base::token_base(token_base const& tb)  //Copy constructor
 	: hash_function(tb.hash_function), tokens(tb.tokens) {}
 
  token_base::token_base(token_base&& tb) noexcept//Moving constructor 
@@ -19,7 +19,7 @@ token_base::token_base(token_base& tb)  //Copy constructor
 	tb.~token_base();
 }
 
-token_base token_base::operator=(token_base& tb) { //operator=
+token_base token_base::operator=(token_base const& tb) { //operator=
 	this->tokens = tb.tokens;
 	this->hash_function = tb.hash_function;
 	return *this;
@@ -36,39 +36,39 @@ token_base::~token_base() {} //Destructor
 
 
 //Work with tokens
-void token_base::add_token(std::string name, std::string password, std::string salt) { //Adding of token by name, password and "salt"
-	tokens.insert(hash_function(name, password, salt));
+void token_base::add_token(std::string const& name, std::string const& password, std::string const& salt) { //Adding of token by name, password and "salt"
+	this->tokens.insert(hash_function(name, password, salt));
 }
 
-void token_base::add_token(std::string token) { //Adding of token by prepared token
+void token_base::add_token(std::string const& token) { //Adding of token by prepared token
 	tokens.insert(token);
 }
 
-bool token_base::it_has(std::string token) const { //Checking for existence of token by prepared token
+bool token_base::it_has(std::string const& token) const { //Checking for existence of token by prepared token
 	return tokens.find(token) != tokens.end();
 }
 
-bool token_base::it_has(std::string name, std::string password, std::string salt) const { //Checking for existence of token by name, password and "salt"
+bool token_base::it_has(std::string const& name, std::string const& password, std::string const& salt) const { //Checking for existence of token by name, password and "salt"
 	return tokens.find(hash_function(name, password, salt)) != tokens.end();
 }
 
-auto token_base::find_token(std::string token) const { //Search of token by prepared token
+auto token_base::find_token(std::string const& token) const { //Search of token by prepared token
 	auto it = tokens.find(token);
 	return it;
 }
 
-auto token_base::find_token(std::string name, std::string password, std::string salt) const { //Search of token by name, password and "salt"
+auto token_base::find_token(std::string const& name, std::string const& password, std::string const& salt) const { //Search of token by name, password and "salt"
 	auto it = tokens.find(hash_function(name, password, salt));
 	return it;
 }
 
-void token_base::delete_token(std::string name, std::string password, std::string salt) { //Deleting of token by name, password and "salt"
+void token_base::delete_token(std::string const& name, std::string const& password, std::string const& salt) { //Deleting of token by name, password and "salt"
 	auto it = tokens.find(hash_function(name, password, salt));
 	if (it == tokens.end()) return;
 	tokens.erase(it);
 }
 
-void token_base::delete_token(std::string token) { //Deleting of token by prepared token
+void token_base::delete_token(std::string const& token) { //Deleting of token by prepared token
 	auto it = tokens.find(token);
 	if (it == tokens.end()) return;
 	tokens.erase(it);
@@ -88,7 +88,7 @@ token_base::tokenb_iter token_base::cend() const { //Constable end of token cont
 
 //P.S.: should add file input by name, password and "salt"
 
-void token::file_input(std::string path_to_file, token_base& token_base) { //File input
+void token::file_input(std::string const& path_to_file, token_base& token_base) { //File input
 	std::ifstream file_input;
 	file_input.open(path_to_file);
 	if (!file_input.is_open()) return; //File opens check
@@ -101,7 +101,7 @@ void token::file_input(std::string path_to_file, token_base& token_base) { //Fil
 	file_input.close();
 }
 
-void token::file_output(std::string path_to_file, token_base& token_base) { //File output
+void token::file_output(std::string const& path_to_file, token_base const& token_base) { //File output
 	std::ofstream file_out;
 	file_out.open(path_to_file);
 	for (auto i = token_base.cbegin(); i != token_base.cend(); ++i) {
--- tokenbase/token_base.h
+++ tokenbase/token_base.h
@@ -9,37 +9,37 @@ class token_base {
 //"Salt" - a string which doesn't allow dehashing the token only by name and password
 
 public: //Redycing of types
-	using hash_func_t = std::string(*)(std::string name, std::string password, std::string salt);
+	using hash_func_t = std::string(*)(std::string const& name, std::string const& password, std::string const& salt);
 	using token_cont_t = std::set<std::string>;
 	using tokenb_iter = token_cont_t::iterator;
 
 public: //Constructors and destructors
 	explicit token_base(hash_func_t hash_function);
-	explicit token_base(hash_func_t hash_function, token_cont_t& tokens);
+	explicit token_base(hash_func_t hash_function, token_cont_t const& tokens);
 	explicit token_base(hash_func_t hash_function, token_cont_t&& tokens);
-	token_base(token_base& tb);
+	token_base(token_base const& tb);
 	token_base(token_base&& tb) noexcept;
 	~token_base();
 
-	token_base operator=(token_base& tb);
+	token_base operator=(token_base const& tb);
 	token_base operator=(token_base&& tb) noexcept;
 
 public: //Work with tokens
 	//Token adding
-	void add_token(std::string name, std::string password, std::string salt);
-	void add_token(std::string token);
+	void add_token(std::string const& name, std::string const& password, std::string const& salt);
+	void add_token(std::string const& token);
 
 	//Token checking
-	bool it_has(std::string token) const;
-	bool it_has(std::string name, std::string password, std::string salt) const;
+	bool it_has(std::string const& token) const;
+	bool it_has(std::string const& name, std::string const& password, std::string const& salt) const;
 
 	//Token search
-	auto find_token(std::string token) const;
-	auto find_token(std::string name, std::string password, std::string salt) const;
+	auto find_token(std::string const& token) const;
+	auto find_token(std::string const& name, std::string const& password, std::string const& salt) const;
 
 	//Token deleting
-	void delete_token(std::string name, std::string password, std::string salt);
-	void delete_token(std::string token);
+	void delete_token(std::string const& name, std::string const& password, std::string const& salt);
+	void delete_token(std::string const& token);
 
 public://Access to container
 	tokenb_iter cbegin() const;
@@ -51,8 +51,8 @@ private:
 };
 
 namespace token { //Functions for work with token_base class
-	void file_input(std::string path_to_file, token_base& token_base);
-	void file_output(std::string path_to_file, token_base& token_base);
+	void file_input(std::string const& path_to_file, token_base& token_base);
+	void file_output(std::string const& path_to_file, token_base const& token_base);
 }
 
 #endif
